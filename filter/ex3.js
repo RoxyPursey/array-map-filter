c5d2de42151b47fcbc437518c7feabe98de07c59
RoxyPursey <roxy.pursey@gmail.com>
2020-04-02 14:49:54 +0100

    completed filter ex3

--- filter/ex3.js
+++ filter/ex3.js
@@ -32,8 +32,25 @@ Expected OUTPUT for these sample arguments:
   ]
 */
 
+const products = [
+  { name: 'iPhone X', price: 1159 },
+  { name: 'Xiaomi Mi A2', price: 209 },
+  { name: 'Samsung Galaxy Note 9', price: 992 },
+  { name: 'Huawei P20', price: 480 },
+  { name: 'Huawei P20 Pro', price: 649 }
+]
+
+const maxPrice = 500
+
 function filterOnPrice(products, maxPrice) {
+  const cheaperPhone = products.filter(phone => {
+        if (phone.price <= maxPrice){
+          return {name: phone.name, price: phone.price};
+        }
+  })
+  return cheaperPhone; 
 }
+
 
 // DON'T TOUCH THIS!
 module.exports = filterOnPrice;
